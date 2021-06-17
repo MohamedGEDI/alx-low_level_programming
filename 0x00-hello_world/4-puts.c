370347f0b3cc0afbdc7b5ecceb8ca84ee742b8fa
MohamedGEDI <gedimohammed@gmail.com>
2021-06-17 05:58:57 -0700

    puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 */
 
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
+{	char str[52] ="\"Programming is like building a multilingual puzzle";
+	puts(str"\n");
 	return (0);
 }
