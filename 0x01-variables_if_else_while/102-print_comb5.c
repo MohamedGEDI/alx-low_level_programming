b83f867d578d0d25a8bbdd427ab7b204e8464856
MohamedGEDI <gedimohammed@gmail.com>
2021-06-18 12:12:39 -0700

    nested loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,13 @@ int x;
 int y;
 for (x = 0; x < 99; x++)
 {
-for (y = 0; x < 100; y++)
+for (y = 0; y < 100; y++)
 {
 putchar((x / 10) + '0');
 putchar((x % 10) + '0');
 putchar(' ');
 putchar((y / 10) + '0');
 putchar((y % 10) + '0');
-if (x == 98)
-{
-break;
-}
 putchar(',');
 putchar(' ');
 }
