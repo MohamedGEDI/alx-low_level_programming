ccdfe86a168062301baaa7ec9b4b370083f2f92b
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 08:41:38 -0700

    0-9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * times_table - 0-9 multiplication  table
+ * Return: 0
+*/
+void times_table(void)
+{
+	int row;
+	int col;
+	int res;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			res = row * col;
+			_putchar(res + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+
