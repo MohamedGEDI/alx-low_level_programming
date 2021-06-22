651d9bd42d7a9140b7dbdd4d1c4fc62efe0682cc
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 04:52:38 -0700

    Find lowercase letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 /**
  * _islower - checks if ascii is lower case
+ * c - is of type integer
+ * Return: 0 or 1
 */
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
