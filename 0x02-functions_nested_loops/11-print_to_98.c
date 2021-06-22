33281fb0e2e32e260da831060cbba96b85376ce7
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 10:50:00 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,11 +18,11 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 	}
-	if (n == 98)
+	else if(n == 98)
 	{
 		printf("%d", n);
 	}
-	if (n > 98)
+	else if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
