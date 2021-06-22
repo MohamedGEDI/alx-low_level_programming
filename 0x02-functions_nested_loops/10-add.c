8f5553a08b380a71efe0ad61a49cc8ffc00f35bc
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 10:04:10 -0700

    Functoin to add 2 nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *add - function for adding 2 numbers
+ *@a: first num
+ *@b: secon num
+ *Return: returns the result
+*/
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
