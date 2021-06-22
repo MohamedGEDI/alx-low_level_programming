e9dcf71ef75ce474f265d4946854d087aabe82f4
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 02:46:00 -0700

    Header files

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,13 +5,15 @@
 */
 int main(void)
 {
-	char a[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
-	char b;
-
-	for (int i = 0; i < 10; i++)
-	{
-		b = a[i];
-		_putchar(b);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
