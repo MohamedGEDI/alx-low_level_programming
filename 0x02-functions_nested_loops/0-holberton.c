591cdd8ca4a5314aadb79a82fb334c1fdce4ebc5
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 02:01:19 -0700

    using _putchar as header file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#include "_putchar.h"
 /**
  * main - print holberton
  * Return: always 00
 */
 int main(void)
 {
-	printf("Holberton");
+	char a[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
+	char b;
+
+	for (int i = 0; i < 10; i++)
+	{
+		b = a[i];
+		_putchar(b);
+	}
 	return (0);
 }
