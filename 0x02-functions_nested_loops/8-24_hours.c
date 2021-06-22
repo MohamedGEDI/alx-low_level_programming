2bbdda72c2212b94acbaa5c26bc860a2ed948464
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 08:18:13 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * jack_bauer - print 24 hours
+ * Return: 0
+*/
+void jack_bauer(void)
+{
+	int fh;
+	int lh;
+	int fm;
+	int lm;
+
+	for (fh = 0; fh <= 2; fh++)
+	{
+		for (lh = 0; lh <= 9; lh++)
+		{
+			for (fm = 0; fm <= 5; fm++)
+			{
+				for (lm = 0; lm <= 9; lm++)
+				{
+					_putchar(fh + '0');
+					_putchar(lh + '0');
+					_putchar(':');
+					_putchar(fm + '0');
+					_putchar(lm + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
