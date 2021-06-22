eb4c07a797d59fd814281f855e540b22a1fc462e
MohamedGEDI <gedimohammed@gmail.com>
2021-06-22 07:23:19 -0700

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last number
+ * @n: inout parameter
+ * Return: 0 or 1
+*/
+int print_last_digit(int n)
+{
+	int mod;
+
+	mod = n % 10;
+	return (mod);
+}
