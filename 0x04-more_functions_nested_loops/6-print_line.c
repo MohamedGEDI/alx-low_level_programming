e00498015035e9e7305c5edf6db559ee8f47849b
MohamedGEDI <gedimohammed@gmail.com>
2021-06-24 10:49:57 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints the number of underscores
+ * @n: input number
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
