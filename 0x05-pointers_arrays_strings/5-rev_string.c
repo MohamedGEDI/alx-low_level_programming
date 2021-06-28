e91821f49e181fd2feaf9a6991d3f020434b078e
MohamedGEDI <gedimohammed@gmail.com>
2021-06-28 09:35:19 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *rev_string - print string reverse
+ *@s: string pointer
+*/
+void rev_string(char *s)
+{
+	int strlen;
+	int c;
+	char *begin;
+	char *end;
+	char tmp;
+	int i = 10;
+
+	begin = s;
+	end = s;
+
+	for (c = 0; c < (i - 1); c++)
+		end++;
+	for (c = 0; c < i / 2; c++)
+	{
+		tmp = *end;
+		*end = *begin;
+		*end = tmp;
+		begin++;
+		end--;
+	}
+}
