e0b01711df7d63af9b78152e94779615829fed39
MohamedGEDI <gedimohammed@gmail.com>
2021-06-28 08:26:37 -0700

    length of str

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
- *
- *
+ *_strlen - finds the length of a string
+ *@s: parameter
+ *Return: returns the lenght of the string
 */
 int _strlen(char *s)
 {
-	strlen(*s);
+	int c;
+
+	c = 0;
+	while (s[c] != '\0')
+		c++;
+
+	return (c);
 }
