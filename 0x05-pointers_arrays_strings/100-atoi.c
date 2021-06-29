3b5d37a15c38d5976de8ac943f9398e3c57dec54
MohamedGEDI <gedimohammed@gmail.com>
2021-06-29 09:56:04 -0700

    a to int func

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ *
+ *
+*/
+int _atoi(char *s)
+{
+	int i;
+	int c;
+	int len;
+
+	len = strlen(s);
+	
+	c = atoi(s);
+
+	return (c);
+}
