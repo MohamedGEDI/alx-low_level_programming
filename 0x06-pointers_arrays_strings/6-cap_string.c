0b09e0f043c852730d93eba69653c55cb64997be
MohamedGEDI <gedimohammed@gmail.com>
2021-07-01 02:09:21 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *str)
 				str[i] = str[i] - 32;
 			continue;
 		}
-		if (str[i] == ' ' || str[i] == '.')
+		if (str[i] == '.')
 		{
 			++i;
 			if (str[i] >= 'a' && str[i] <= 'z')
@@ -25,12 +25,15 @@ char *cap_string(char *str)
 				str[i] = str[i] - 32;
 				continue;
 			}
-		}
-		else
+		if (str[i] == ' ')
 		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
 		}
+		
 	}
 	return (str);
 }
