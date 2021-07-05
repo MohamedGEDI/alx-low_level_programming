c2f5e5a33e41714acfcd9cdf2ceaa2e0e0d4c40f
MohamedGEDI <gedimohammed@gmail.com>
2021-07-05 09:30:07 -0700

    search for char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strchr - find occurence 
+ *@s: string array
+ *@c: char to be searched
+ *Return: first occurence
+*/
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (s);
+}
