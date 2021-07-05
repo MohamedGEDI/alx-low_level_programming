3d5ccd263492c8dc8ee669e3c1873a40f4b14b40
MohamedGEDI <gedimohammed@gmail.com>
2021-07-05 13:04:09 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_chessboard - prints chessboard
+ *a: paramerter
+*/
+void print_chessboard(char arr(*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", arr *((a + i) + j);
+		}
+	}
+}
