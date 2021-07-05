39d03ab862c2149e697c254fe69f6bb805991f02
MohamedGEDI <gedimohammed@gmail.com>
2021-07-05 13:15:23 -0700

    pointer of pointer

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  *set_string - pointer to a char
- *@s: pointer of a pointer 
- *@to: char 
+ *@s: pointer of a pointer
+ *@to: char
 */
 void set_string(char **s, char *to)
 {
