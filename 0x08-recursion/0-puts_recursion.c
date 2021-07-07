f081ee70557bd4aa141bf630d00c4c104f60b57d
MohamedGEDI <gedimohammed@gmail.com>
2021-07-07 08:52:11 -0700

    Recurssiioonnn baby

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,21 +2,19 @@
 #include "holberton.h"
 #include <string.h>
 /**
- *
- *
- *
+ *_puts_recursion - prints string in recursion
+ *@s: string
 */
 void _puts_recursion(char *s)
 {
-	
-	if(*s)
+
+	if (*s)
 	{
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
-	else 
+	else
 	{
 		printf("\n");
 	}
 }
-
