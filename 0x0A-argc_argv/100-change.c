c045586e749f177aed014e9e2b3033b0d122c443
MohamedGEDI <gedimohammed@gmail.com>
2021-07-12 07:44:12 -0700

    you got change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ *main - main to print contents of argv
+ *@argc: number of commands
+ *@argv: command strings entered
+ *Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int amount, i, coin, rem;
+	int changes[] = {25, 10, 5, 2 , 1};
+	int total = 0;
+
+	amount = atoi(argv[1]);
+	if (argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (amount == 0)
+	{
+		printf("%d\n", amount);
+	}
+	else 
+	{
+		for(i = 0; i < 5; i++)
+		{
+			if (amount >= changes[i])
+			{
+				coin = amount / changes[i];
+				rem = amount % changes[i];
+				amount = rem;
+				total += coin;
+			}
+		}
+		printf("%d\n", total);
+	}	
+	return (0);
+}
