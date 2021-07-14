920cd9b965f2d907d7337d97f4d0fe565065d19e
MohamedGEDI <gedimohammed@gmail.com>
2021-07-14 08:50:00 -0700

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ *str_concat - concat two strings
+ *@s1: char in the array
+ *@s2: char in the array
+ *Return: returns array
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	int len1;
+	int len2;
+
+	len1 = s1 == NULL ? 0 : strlen(s1);
+	len2 = s2 == NULL ? 0 : strlen(s2);
+
+	ar = (char *)malloc((len1 + len2) + 1);
+
+	if (ar == NULL)
+		return (NULL);
+
+	else
+	{
+		strcat(s1, s2);
+		*ar = *s1;
+
+		return (ar);
+	}
+
+}
