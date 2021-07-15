b21b8d132e04a4025e9a7f291e5a1c74063fe4a5
MohamedGEDI <gedimohammed@gmail.com>
2021-07-15 11:46:06 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+##include "holberton.h"
 #include <stdlib.h>
 /**
  *_calloc - non type malloc space
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,2 +1,3 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
