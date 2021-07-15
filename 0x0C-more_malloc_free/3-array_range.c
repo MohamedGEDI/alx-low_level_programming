a6dda53c04ebb5fc4e13421ae0136ccdb6c6683e
MohamedGEDI <gedimohammed@gmail.com>
2021-07-15 12:22:23 -0700

    range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,12 +10,13 @@
 int *array_range(int min, int max)
 {
 	int i;
-
+	int *p;	
 	i = 0;
+	
 	if (min > max)
 		return (NULL);
 
-	int *p = (int *)malloc(((max - min) + 1) * sizeof(int));
+        p = malloc(((max - min) + 1) * sizeof(int));
 
 	if (p == NULL)
 		return (NULL);
