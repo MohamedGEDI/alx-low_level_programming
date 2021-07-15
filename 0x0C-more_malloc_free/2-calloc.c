28da9a34837a550913c000242474a5376d2b45b9
MohamedGEDI <gedimohammed@gmail.com>
2021-07-15 11:25:53 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_calloc - non type malloc space
+ *@nmemb: array
+ *@size: size
+ *Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(sizeof(int) * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		*((int *)p + 1) = 0;
+	}
+
+	return (p);
+}
