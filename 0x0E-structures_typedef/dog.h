dfab57c7313bf7a5dfbf00c105c888f9ad45c2fa
MohamedGEDI <gedimohammed@gmail.com>
2021-07-20 09:33:32 -0700

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
+/**
+ * struct dog - struct dog
+ * @name: member
+ * @age: member
+ * @owner: Memebr
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
