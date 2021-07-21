316386c079d23db25c84dd744708aa1b3049bb32
MohamedGEDI <gedimohammed@gmail.com>
2021-07-21 03:10:15 -0700

    return a number that does

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,18 +2,19 @@
 #include "function_pointers.h"
 /**
  *int_index - return first number that doesnt return a 0 in cmp
- *@array: array 
+ *@array: array
  *@size: ln of array
  *@cmp: compare function
+ *Return: first num that doesnt compare
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, c, res;
-	
+
 	if (size <= 0)
 		return (-1);
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		c = cmp(array[i]);
 		if (c != 0)
