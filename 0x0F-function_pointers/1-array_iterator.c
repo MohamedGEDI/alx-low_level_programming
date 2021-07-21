93ccfe8c7b049479946f6ba350d04372fdd856ea
MohamedGEDI <gedimohammed@gmail.com>
2021-07-21 02:38:32 -0700

    call back array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- *
- *
- *
- *
+ *array_iterator - goes through the array
+ *@array: array
+ *@size: size of array
+ *@action: call back func
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
