77eefaf5cb6b10cea4a0d8c47cf6723b4e3a8edd
MohamedGEDI <gedimohammed@gmail.com>
2021-07-22 01:03:53 -0700

    first variadic func

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *sum_them_all - adds all the arguments given
+ *@n: number of ints
+ *Return: sum or 0
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	va_list args;
+
+	if ( n == 0)
+		return (0);
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(args, int);
+	}
+	return (sum);
+}
