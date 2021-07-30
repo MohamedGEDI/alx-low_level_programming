9da8df5a861926c4a5549a772e796d7d54295b6c
MohamedGEDI <gedimohammed@gmail.com>
2021-07-30 11:00:50 -0700

    exe before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * print_before_main - print this function beforemain
+*/
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
