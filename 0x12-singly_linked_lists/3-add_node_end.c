492cdd03722d16b696c43ac41a86ee05c9a6338e
MohamedGEDI <gedimohammed@gmail.com>
2021-07-30 04:23:32 -0700

    add list to end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ *add_node_end - function to addnode to the tail
+ *@head:current head of the linked list
+ *@str: string
+ *Return: address to new pointer
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = (list_t *)malloc(sizeof(list_t));
+
+	list_t *last = *head;
+
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+		return (new);
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	return (new);
+}
