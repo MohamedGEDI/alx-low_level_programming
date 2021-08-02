d6051884d451471ce1935a0db290580deaacd7e8
MohamedGEDI <gedimohammed@gmail.com>
2021-08-02 01:42:08 -0700

    print singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *print_listint - prints the int held in a linked list
+ *@h: linked list
+ *Return: number of nodes 
+*/
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return(count);
+}	
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * struct list_s - singly inked l,ist
+ * @str: string
+ * @len: length of string
+ * @next: points to next node
+*/
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+#endif
