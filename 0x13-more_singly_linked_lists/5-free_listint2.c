afaf9de2cfeef17255103fd16bbc0d8d412741f7
MohamedGEDI <gedimohammed@gmail.com>
2021-08-03 02:01:08 -0700

    free  list and set had to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list and set head to null
+ * @head: pointer to a pointer 
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+	current = *head;
+
+	if (current == NULL)
+		return ;
+	else
+	{
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current);
+			current = next;
+		}
+		current = NULL;
+	}
+}
