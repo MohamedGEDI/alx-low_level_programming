9c8a18820caad4f22d2babbf27befe4acdfc633a
MohamedGEDI <gedimohammed@gmail.com>
2021-08-03 01:33:41 -0700

    delete linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ *free_listint - deletes a linked list
+ *head: head of the list 
+*/
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}
