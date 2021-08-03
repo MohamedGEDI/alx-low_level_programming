ff7634515dca5c7d0fb5edf7146d28701c1a6892
MohamedGEDI <gedimohammed@gmail.com>
2021-08-03 03:10:04 -0700

    pop the head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
  *pop_listint - remove the head node
- *@head: linked list 
+ *@head: linked list
  *Return: data of deleted node
 */
 int pop_listint(listint_t **head)
 {
 	int n;
 	listint_t *temp = *head;
+
 	n = temp->n;
 	*head = temp->next;
 	free(temp);
