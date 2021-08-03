debc4c7bf5133ac700e878f1272a14b23bcc269f
MohamedGEDI <gedimohammed@gmail.com>
2021-08-03 06:22:58 -0700

    delete at node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,6 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		(*head) = current->next;
 		free(current);
+		current = NULL;
 		return (1);
 	}
 	else
@@ -28,6 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		prev->next = current->next;
 		free(current);
+		current = NULL;
 		return (1);
 	}
 }
