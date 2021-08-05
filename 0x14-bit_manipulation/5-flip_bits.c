991df73b9126db367e5a6f1c713c038e693065f8
MohamedGEDI <gedimohammed@gmail.com>
2021-08-05 09:56:01 -0700

    flip bıts

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * flip_bits - flip
+ * @n: nums
+ * @m: number of 
+ * Return: flipped bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int move = sizeof(unsigned long int) * 8;
+	int dist = 0;
+
+	while(move--)
+		dist = dist + (n >> move & 1) != (m >> move & 1);
+
+	return (dist);
+}
