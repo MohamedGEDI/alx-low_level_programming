4922c37b59280cc1977f549d23bf6a8c2b388f2f
MohamedGEDI <gedimohammed@gmail.com>
2021-09-17 10:03:54 -0700

    get node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_dnodeint - get node at
+ * @head: linked list
+ * @index: get node at index
+ * Return: node atindex
+*/
+dlistint_t *get_dnodeint(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *count = head;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (count != NULL)
+	{
+		if (i == index)
+		{
+			return (count);
+		}
+		count = count->next;
+		i++;
+	}
+	return (NULL);
+}
