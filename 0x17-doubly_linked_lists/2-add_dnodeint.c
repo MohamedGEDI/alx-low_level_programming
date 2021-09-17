9208d33487bab88bfaa2995bcdfad389305dfeb1
MohamedGEDI <gedimohammed@gmail.com>
2021-09-17 02:18:43 -0700

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,9 +13,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = (*head);
+	new->prev = NULL
 	(*head) = new;
 	if (*head != NULL)
-		return (new);
-	else
-		return (NULL);
+		*head->prev = new;
+	return new;
 }
