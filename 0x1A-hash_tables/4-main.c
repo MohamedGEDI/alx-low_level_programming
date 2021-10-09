ee542b9ba3cf577a42d19bdde8001a5a6d86fc7e
MohamedGEDI <gedimohammed@gmail.com>
2021-10-09 12:12:52 -0700

    fix

--- 0x1A-hash_tables/4-main.c
+++ 0x1A-hash_tables/4-main.c
@@ -3,18 +3,11 @@
 int main(void)
 {
 	hash_table_t *ht;
-	char *value;
 
 	ht = hash_table_create(1024);
 	hash_table_set(ht, "Mohamed", "Gedis son");
 	hash_table_set(ht, "Whatsapp", "Facebook");
 	hash_table_set(ht, "Youtube", "google");
-
-	value = hash_table_get(ht, "Whatsapp");
-	printf("%s\n",value);
-	value = hash_table_get(ht, "Mohamed");
-	printf("%s\n",value);
-	value = hash_table_get(ht, "Youtube");
-	printf("%s\n",value);
+	hash_table_print(ht);
 	return (0);
 }
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print out ht 
+ * @ht - ht
+*/
+void hash_table_print(hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int i;
+	unsigned int flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for(i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (flag == 1 && node != NULL)
+			printf(", ");
+		if (node != NULL)
+		{
+			printf("%s: %s",node->key, node->value);
+			flag = 1;
+			continue;
+		}
+		while (node != NULL)
+		{
+			flag = 1;
+			printf("%s: %s",node->key, node->value);
+			if (node->next != NULL)
+				printf(", ");
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
